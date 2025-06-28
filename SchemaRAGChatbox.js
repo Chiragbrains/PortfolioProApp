52804361c10a81e80c5698337c44ed60b1719b56
“Chirag” <chiragbrains@yahoo.com>
2025-06-28 11:58:01 -0400

    lambda (YFinance) RAG

--- SchemaRAGChatbox.js
+++ SchemaRAGChatbox.js
@@ -35,8 +35,8 @@ import {
 import { SchemaRAGChatbox as SchemaRAGChatboxUI } from './SchemaRAGChatbox.jsx';
 import { getDynamicAlphaVantageResponse, runAlphaVantagePipeline } from './services/alphaVantageLLMService.js'; // Corrected path
 
-import { GROQ_API_KEYS } from '@env';
-console.log('GROQ_API_KEY loaded in SchemaRAGChatbox:', GROQ_API_KEYS);
+import { GROQ_API_KEY } from '@env';
+console.log('GROQ_API_KEY loaded in SchemaRAGChatbox:', GROQ_API_KEY);
 
 // Import YFinance handler
 import YFinanceHandler from './yfinance_handler.js';
